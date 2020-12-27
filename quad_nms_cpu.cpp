0a82dbefa1461c73213a7b1dc6e5eef7eab41f69
zhouyuangan <1401499346@qq.com>
2020-12-27 12:20:23 +0800

    slice实现有bug

--- quad_nms_cpu.cpp
+++ quad_nms_cpu.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 using namespace ClipperLib;
 
+void slice(const float* boxes, int box_dim, float* out, const int* indexes, int index_num);
+
 /// <summary>
 /// 计算两个四边形的交并比
 /// </summary>
@@ -27,7 +29,7 @@ void box_to_path(const float* box, Path& path);
 /// </summary>
 /// <param name="boxes"></param>
 /// <param name="conf_thresh"></param>
-void nms_cpu(float* boxes, int box_num, float threshold, int* keep, int& keep_num);
+void nms_cpu(float* boxes, int box_num, float threshold, int* keep, size_t& keep_num);
 
 void box_to_path(const float* box, Path& path) {
 	for (size_t i = 0; i < 4; i++)
@@ -71,7 +73,7 @@ void comput_iou(const float* box1, const float* box2, float* iou) {
 	float inter_area = Area(inter_solution[0]); // 计算交面积
 	float union_area = Area(union_solution[0]); // 计算并面积
 
-	*iou = inter_area / (union_area + 1e-16); // 防止下溢
+	*iou = inter_area / std::max(union_area, 1.f); // 防止下溢
 	return;
 }
 
@@ -83,7 +85,7 @@ void comput_iou(const float* box1, const float* box2, float* iou) {
 /// <param name="threshold">iou的阈值</param>
 /// <param name="keep">索引列表</param>
 /// <param name="keep_num">保留框的个数</param>
-void nms_cpu(float* boxes, int box_num, float threshold, int* keep, int& keep_num) {
+void nms_cpu(float* boxes, int box_num, float threshold, int* keep, size_t& keep_num) {
 	int _keep_num = 0;
 	for (size_t i = 0; i < box_num; i++)
 	{
@@ -96,15 +98,16 @@ void nms_cpu(float* boxes, int box_num, float threshold, int* keep, int& keep_nu
 		while (j<box_num)
 		{
 			if (boxes[j * 9 + 8] == -1.0) {
+				j = j + 1;
 				continue;
 			}
 			float* next_box = &boxes[j * 9];
 			float _iou;
 			comput_iou(current_box, next_box, &_iou);
 			// 如果iou大于阈值，说明重合度太高，可以认为是同一个对象，不保留
+			printf("iou: %f\n", _iou);
 			if (_iou >= threshold) {
 				boxes[j * 9 + 8] = -1.0; // 如果重合度太高，则将该box的置信度设置为0
-				continue;
 			}
 			
 			j = j + 1;
@@ -117,6 +120,20 @@ void nms_cpu(float* boxes, int box_num, float threshold, int* keep, int& keep_nu
 	// 整个程序结束后，将保留的box的个数返回
 	keep_num = _keep_num;
 	
+	return;
+}
+
+void slice(const float* boxes, int box_dim, float* out, const int* indexes, int index_num) {
+	// 迭代索引列表
+	for (size_t i = 0; i < index_num; i++)
+	{
+		size_t index = indexes[i]; // index指向boxes中的索引
+		for (size_t j = 0; j < box_dim; j++)
+		{
+			// out的长度即是index_num
+			out[i * 8 + j] = boxes[index * 8 + j]; // 将index指向的box数据拷贝给out
+		}
+	}
 	return;
 }
 
@@ -144,20 +161,34 @@ int test_nms()
 
 	// 94,99,155,292,512,219,463,70
 	// 76,159,53,292,485,340,505,105
-	int box_num = 3;
+	int box_num = 6;
 	float boxes[] = {
-		253,285,824,296,833,435,283,413,0.9,
-		215,267,853,296,826,449,246,414,0.8,
-		200,267,824,296,800,449,283,414,0.85,
+		95,71,80,255,332,250,374,80,0.9,
+		43,175,461,171,409,308,55,309,0.8,
+		62,107,417,114,408,227,34,216,0.85,
+		215,426,469,424,474,595,219,595,0.8,
+		175,457,440,460,451,635,178,624,0.75,
+		255,371,264,609,467,618,449,370,0.6
 	};
 	
 	int* keep = new int[box_num];
-	int keep_num = 0;
-	float threshold = 0.7;
+	size_t keep_num = 0;
+	float threshold = 0.3;
 	nms_cpu(&boxes[0], box_num, threshold, keep, keep_num);
 	cout << "number of keep boxes: " << keep_num << endl;
+	float* out = new float[keep_num * 9];
+	slice(boxes, 9, out, keep, keep_num);
+
+	for (size_t i = 0; i < keep_num; i++)
+	{
+		cout << "{" << out[i * 8] << "," << out[i * 8 + 1] << "," << out[i * 8 + 2] << "," << out[i * 8 + 3] \
+			<< "," << out[i * 8 + 4] << "," << out[i * 8 + 5] << "," << out[i * 8 + 6] << "," << out[i * 8 + 7] \
+			<< "," << out[i * 8 + 8] << "}";
+		cout << endl;
+	}
 
 	delete[] keep;
+	delete[] out;
 
 	return 0;
 }
